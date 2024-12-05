a6d4902abb2a65877bf8c0b229fab68b81221c4c
Wai_YangTan <88041819+comcealed@users.noreply.github.com>
2024-12-05 19:48:16 +0800

    Upload Calculator component

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Calculator from "@/components/widgets/Calculator/Calculator";
+
 export default function TestingPage() {
     return (
         <>
@@ -8,6 +10,7 @@ export default function TestingPage() {
                     <h1 className="font-bold text-[30px]">This is the testing page for Weejets.</h1>
                     <p>Just import your created widget here for testing.</p>
                 </div>
+                <Calculator/>
             </main>
         </>
     )
