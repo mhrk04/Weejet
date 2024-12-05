8c510cbe3330c61652db7dc380493f4aee64df94
Johnson Chin <107231772+JohnsonChin1009@users.noreply.github.com>
2024-12-05 20:18:48 +0800

    Update page.tsx

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -7,9 +7,7 @@ export default function TestingPage() {
                 <div className="text-center space-y-3 pt-10">
                     <h1 className="font-bold text-[30px]">This is the testing page for Weejets.</h1>
                     <p>Just import your created widget here for testing.</p>
-
                 </div>
-                <StudyTimer/>\
             </main>
         </>
     )
